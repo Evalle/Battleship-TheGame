27809066e24de5692799a93e65c1bd9972a75731
Evalle <shmarnev@gmail.com>
2014-04-27 16:47:39 +0600

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -29,8 +29,7 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-guess_row = int(raw_input("Guess Row:"))
-guess_col = int(raw_input("Guess Col:"))
+
 
 print ""
 print ship_row
@@ -38,18 +37,24 @@ print ship_col
 print ""
 
 ##### logic 
+for turn in range(4):
+    guess_row = int(raw_input("Guess Row:"))
+    guess_col = int(raw_input("Guess Col:"))
+    
+    if guess_row not in range(5) or guess_col not in range(5):
+        print "Oops, that's not even in the ocean."
 
-if guess_row not in range(5) or guess_col not in range(5):
-    print "Oops, that's not even in the ocean."
+    elif guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sank my battleship!"
 
-elif guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sank my battleship!"
+    elif board[guess_row][guess_col] == "X":
+        print "You guessed that one already."
 
-elif board[guess_row][guess_col] == "X":
-    print "You guessed that one already."
-
-else:
-    print "You missed my battleship!"
-    print ""
-    board[guess_row][guess_col] = "X"
-    print_board(board)
+    else:
+        print "You missed my battleship!"
+        print ""
+        board[guess_row][guess_col] = "X"
+        print_board(board)
+        print ""
+        print "Turn", (turn + 1)
+        print ""
